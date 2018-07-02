caea1309ff946c44e45dc8feaf813f6b9ea949a7
Eshan Saxena <eshan.eshan.saxena0@gmail.com>
2018-07-02 23:32:55 +0530

    7.2

---
+++ script.js
@@ -0,0 +1,13 @@
+/*JS code to print multiples of 5 using for loop */
+var n = 5;
+console.log("Using for loop")
+for (var i = 1; i <= 10; i++) {
+    console.log(n * i);
+}
+/*JS code to print multiples of 5 using while loop */
+var j = 1;
+console.log("Using while loop")
+while (j <= 10) {
+    console.log(n * j);
+    j++;
+}
